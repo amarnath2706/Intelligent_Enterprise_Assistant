1dfeb731b4320f2674c0a64f7a0f568ad38da504
amarnath2706 <amarnath.arjsd@gmail.com>
2026-07-06 20:07:54 +0530

    config_updated

--- app/config.py
+++ app/config.py
@@ -1,2 +1,17 @@
 import os 
 from dotenv import load_dotenv
+
+
+load_dotenv()  # Load environment variables from .env file  
+
+class Settings:
+
+   GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+   QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+   QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+   QDRANT_COLLECTION = "enterprise_assistant_collection" #name of the collection in qdrant database
+   GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+   GROQ_MODEL = "llama-3.3-70b-versatile"
+   #GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")  # Optional fallback API
+
+settings = Settings()
